b1dc17e05776a742b96a856ddb81dea3f92230c4
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2024-08-04 17:24:48 +0200

    Prepare for 0.02 version

--- Board/List.pm
+++ Board/List.pm
@@ -15,7 +15,7 @@ use Tags::HTML::Footer;
 use Tags::HTML::Messages;
 use Tags::HTML::Table::View;
 
-our $VERSION = 0.01;
+our $VERSION = 0.02;
 
 sub _cleanup {
 	my ($self, $env) = @_;
--- Board/Message.pm
+++ Board/Message.pm
@@ -20,7 +20,7 @@ use Tags::HTML::Messages;
 use Tags::HTML::Message::Board;
 use Tags::HTML::Message::Board::Blank;
 
-our $VERSION = 0.01;
+our $VERSION = 0.02;
 
 sub _cleanup {
 	my ($self, $env) = @_;
